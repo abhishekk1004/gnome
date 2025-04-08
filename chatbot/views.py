4d594a81a4681b7d43564df3752e728d0aff13d4
abhishekk1004 <abhiobray143@gmail.com>
2025-04-08 09:20:54 +0545

    updated views

--- chatbot/views.py
+++ chatbot/views.py
@@ -53,6 +53,30 @@
 
 #     return render(request, "chatbot.html")
 
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# import json
+# from .utils.chat import chatbot_response  
+
+# @csrf_exempt
+# def chatbot_view(request):
+#     if request.method == "POST":
+#         try:
+#             data = json.loads(request.body)
+#             text = data.get("message", "")
+
+#             if not text:
+#                 return JsonResponse({"error": "Empty message"}, status=400)
+
+#             response = chatbot_response(text)
+#             return JsonResponse({"answer": response})
+
+#         except Exception as e:
+#             return JsonResponse({"error": str(e)}, status=500)
+
+#     return render(request, "chatbot.html")  
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -75,5 +99,4 @@ def chatbot_view(request):
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
 
-    return render(request, "chatbot.html")  
-
+    return render(request, "chatbot.html")
